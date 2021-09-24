b168740fc392064e2f257f45e882fb6d9950e615
cosmickj <kyungjun1213@gmail.com>
2021-09-24 23:14:32 +0900

    add shine1 directory

---
+++ shine1/covid19_status_sigungu_version1.2.0.py
@@ -0,0 +1,254 @@
+# ! /usr/bin/python3
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+
+from configparser import ConfigParser
+from datetime import datetime
+import pandas as pd
+import time
+import json
+import re
+
+import warnings
+
+warnings.filterwarnings(action="ignore")
+
+import pymysql
+
+pymysql.install_as_MySQLdb()
+from sqlalchemy import create_engine
+
+# 데이터 베이스 연결 설정
+config = ConfigParser()
+config.read("../config/secret.ini")
+
+HOSTNAME = config["appmd_db"]["HOSTNAME"]
+PORT = int(config["appmd_db"]["PORT"])
+USERNAME = config["appmd_db"]["USERNAME"]
+PASSWORD = config["appmd_db"]["PASSWORD"]
+DATABASE = config["appmd_db"]["DATABASE"]
+CHARSET1 = config["appmd_db"]["CHARSET1"]
+CHARSET2 = config["appmd_db"]["CHARSET2"]
+
+con_str = f"mysql+mysqldb://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}?charset={CHARSET1}"
+engine = create_engine(con_str, encoding=CHARSET2)
+
+# 크롬 드라이버 연결 설정
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
+DRIVER_LOCATION = "/usr/bin/chromedriver"
+BINARY_LOCATION = "/usr/bin/google-chrome"
+
+options = Options()
+options.add_argument(f"user-agent={user_agent}")
+options.add_argument("--headless")
+options.add_argument("no-sandbox")
+options.add_argument("--disable-extensions")
+options.binary_location = BINARY_LOCATION
+
+
+def check_sigungun_update(db_table, sido):
+    """해당 시도 데이터 베이스 업데이트 날짜 확인"""
+    sql = f"SELECT MAX(upd_date) FROM {db_table} WHERE SIDO = '{sido}';"
+    sql_df = pd.read_sql(sql, con=engine)
+    latest_update_date = sql_df.iloc[0, 0].date()
+    return latest_update_date
+
+
+def update_table(table, db_table, sido):
+    table.to_sql(name=f"{db_table}", con=engine.connect(), if_exists="append", index=False)
+    print(f"{sido} Updated Complete.")
+
+
+def get_soup():
+    html = driver.page_source
+    return BeautifulSoup(html, "html.parser")
+
+
+def clean_update_date(date_element):
+    std = date_element.text.find("기준")
+    if std == -1:
+        std = None
+    date_element = date_element.text[:std]
+    update_date_text_list = re.findall("\d+.|오전|오후", date_element)
+    update_date_text = "".join(update_date_text_list).strip()
+    update_date_text = re.sub("[년월일\-]", ".", update_date_text)
+    update_date_text = update_date_text + ":00"
+
+    if "오전" in update_date_text:
+        update_date_text += "am"
+    elif "오후" in update_date_text:
+        update_date_text += "pm"
+
+    update_date_text = re.sub("[가-힣]", "", update_date_text)
+    try:
+        update_date = pd.to_datetime(update_date_text, yearfirst=True)
+    except:
+        update_date_text = str(datetime.today().year) + "." + update_date_text
+        update_date = pd.to_datetime(update_date_text, yearfirst=True)
+    return update_date
+
+
+def extract_target_html(current_html_tag):
+    if str(current_html_tag).startswith("<table"):
+        return current_html_tag
+    elif str(current_html_tag).startswith("<div"):
+        return current_html_tag
+    else:
+        current_html_tag = current_html_tag.find_parent()
+        return extract_target_html(current_html_tag)
+
+
+def extract_sigungu_name(target_data_html, tag_for_name, sido_kr):
+    if tag_for_name == None:
+        """시군구 단위를 제공하지 않음: 대구, 세종, 제주"""
+        if sido_kr == "세종":
+            sido_kr = sido_kr + "시"  # database 업데이트 시 삭제
+        sigungu_name_list = [sido_kr]  # global variable
+        return sigungu_name_list
+
+    junky_text_pattern = ("계", "구분", "지역", "증감", "자치구별(지역별)", "코로나19")
+    data_dummy = target_data_html.select(tag_for_name)
+
+    if tag_for_name.startswith("img"):
+        """시군구 명칭을 img 태그 안에 담아둠: 충북"""
+        sigungu_name_list = [
+            data["alt"]
+            for data in data_dummy
+            if not data["alt"].endswith(junky_text_pattern) and len(data["alt"]) > 2
+        ]
+        return sigungu_name_list
+
+    def filter_data(data, pattern):
+        chunk = data.text.split()[0]
+        return chunk if not chunk.endswith(pattern) and len(chunk) >= 2 else None
+
+    sigungu_name_list = [
+        o for data in data_dummy if (o := filter_data(data, junky_text_pattern))
+    ]  # 바다코끼리 연산자(the walrus operator)
+    return sigungu_name_list
+
+
+def extract_covid19_confirmed(target_data_html, tag_for_covid19, covid19_count_start_idx, sigungu_name_list):
+    raw_covid19_confirmed_count_list = target_data_html.select(tag_for_covid19)
+    raw_covid19_confirmed_count_list = raw_covid19_confirmed_count_list[
+        covid19_count_start_idx : covid19_count_start_idx + len(sigungu_name_list)
+    ]
+
+    covid19_confirmed_count_list = []
+    for count in raw_covid19_confirmed_count_list:
+        clean_count_text = re.sub("\(.?\d+\)|\D", "", count.text)
+        if clean_count_text:
+            covid19_confirmed_count_list.append(int(clean_count_text))
+
+    return covid19_confirmed_count_list
+
+
+start_time = time.time()
+
+"""MAIN PROCESS"""
+with open("../config/covid19_sido_info.json", "r", encoding="utf-8") as f:
+    sido_data = json.load(f)
+
+today_date = datetime.today().date()
+driver = webdriver.Chrome(executable_path=DRIVER_LOCATION, options=options)
+driver.implicitly_wait(5)
+
+sido_list = [
+    "seoul",
+    "busan",
+    "daegu",
+    "incheon",
+    "gwangju",
+    "daejeon",
+    "ulsan",
+    "sejong",
+    "gyeonggi",
+    "gangwon",
+    "chungbuk",
+    "chungnam",
+    "jeonbuk",
+    "jeonnam",
+    "gyeongbuk",
+    "gyeongnam",
+    "jeju",
+]
+for sido in sido_list:
+    try:
+        db_table = "covid19_kr_by_Municipality"
+        sido_kr = sido_data[sido]["sido_kr"]
+
+        db_sigungu_latest_update_date = check_sigungun_update(db_table, sido_kr)
+        if db_sigungu_latest_update_date == today_date:
+            print(f"{sido_kr} is already updated in database")
+            continue
+
+        url = sido_data[sido]["url"]
+        driver.get(url)
+        driver.refresh()
+
+        # 지자체 홈페이지 업데이트 기준 날짜 가져오기
+        css_selector_for_update_date = sido_data[sido]["css_selector_for_update_date"]
+        date_element = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, css_selector_for_update_date))
+        )
+        update_date = clean_update_date(date_element)
+
+        if update_date != today_date:
+            print(f"{sido_kr} 홈페이지에 코로나 현황이 아직 업데이트 되지 않았습니다.")
+            continue
+
+        soup = get_soup()
+        target_text = sido_data[sido]["target_text"]
+        target_text_html = soup.find(string=re.compile(target_text)).find_parent()
+        target_data_html = extract_target_html(target_text_html)
+
+        # 해당 시도의 시군구 이름 가져오기
+        tag_for_name = sido_data[sido]["tag_for_name"]
+        sigungu_name_list = extract_sigungu_name(target_data_html, tag_for_name, sido_kr)
+        sigungu_name_list = list(dict.fromkeys(sigungu_name_list))  # remove duplicate
+
+        # 시군구별 누적 코로나 확진자 현황 가져오기
+        tag_for_covid19 = sido_data[sido]["tag_for_covid19"]
+        covid19_count_start_idx = sido_data[sido]["covid19_count_start_idx"]
+        covid19_confirmed_list = extract_covid19_confirmed(
+            target_data_html, tag_for_covid19, covid19_count_start_idx, sigungu_name_list
+        )
+
+        covid19_confirmed_by_sigungu = pd.DataFrame(columns=["SIDO", "SIGUN", "TOT_NUM", "UPD_DATE"])
+        covid19_confirmed_by_sigungu["SIGUN"] = sigungu_name_list
+        covid19_confirmed_by_sigungu["TOT_NUM"] = covid19_confirmed_list
+        covid19_confirmed_by_sigungu["SIDO"] = sido_kr
+        covid19_confirmed_by_sigungu["UPD_DATE"] = update_date
+
+        update_table(covid19_confirmed_by_sigungu, db_table, sido_kr)
+    except Exception as e:
+        print(e)
+
+    """
+    [행정구역 현황]
+    서울: 25개 중 25개 제공
+    부산: 16개 중 16개 제공
+    대구: 8개 중 0개 제공 (통합)
+    인천: 10개 중 10개 제공
+    광주: 5개 중 5개 제공
+    대전: 5개 중 5개 제공
+    울산: 5개 중 5개 제공
+    세종: 1개 중 1개 제공
+    경기: 31개 중 31개 제공
+    강원: 18개 중 18개 제공
+    충북: 11개 중 11개 제공
+    충남: 15개 중 15개 제공
+    전북: 14개 중 14개 제공
+    전남: 22개 중 22개 제공
+    경북: 23개 중 23개 제공
+    경남: 18개 중 18개 제공
+    제주: 2개 중 0개 제공 (통합)
+    """
+
+print("time:", time.time() - start_time)
+
+driver.quit()
---
+++ shine1/covid19_vaccine_kr_version1.1.0.py
@@ -0,0 +1,129 @@
+# ! /usr/bin/python3
+print("Start!")
+
+import requests
+
+from configparser import ConfigParser
+from datetime import datetime, timedelta
+import pandas as pd
+import xmltodict
+import json
+
+from sqlalchemy import create_engine
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
+# <-------------------Setting Database------------------->
+config = ConfigParser()
+config.read("../config/secret.ini")
+
+HOSTNAME = config["appmd_db"]["HOSTNAME"]
+PORT = int(config["appmd_db"]["PORT"])
+USERNAME = config["appmd_db"]["USERNAME"]
+PASSWORD = config["appmd_db"]["PASSWORD"]
+DATABASE = config["appmd_db"]["DATABASE"]
+CHARSET1 = config["appmd_db"]["CHARSET1"]
+CHARSET2 = config["appmd_db"]["CHARSET2"]
+
+con_str = f"mysql+mysqldb://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}?charset={CHARSET1}"
+engine = create_engine(con_str, encoding=CHARSET2)
+conn = engine.connect()
+
+kosis_apiKey = config["kosis_api"]["apiKey"]
+
+
+def check_db_update():
+    sql = "SELECT MAX(upd_date) FROM `vaccine_region_status`;"
+    sql_df = pd.read_sql(sql, con=engine)
+    latest_upd_date = sql_df.iloc[0, 0].date()
+    return latest_upd_date
+
+
+def upload_table(table):
+    table.to_sql(name="vaccine_region_status", con=conn, if_exists="append", index=False)
+    print("Updated Complete.")
+
+
+def get_data_from_xml(url):
+    response = requests.get(url).content
+    data = xmltodict.parse(response)
+    return data
+
+
+def get_update_date(data):
+    raw_upd_date = pd.to_datetime(data["response"]["body"]["dataTime"].split()[0])
+    upd_date = raw_upd_date + timedelta(days=1)  # 시간을 전날 24시 형태로 제공하기에 하루를 더해준다
+    return upd_date
+
+
+def create_base_table(data, column):
+    dict_data = json.loads(json.dumps(data, ensure_ascii=False))["response"]["body"]["items"]["item"]
+    base_table = pd.DataFrame(dict_data)
+    base_table = base_table.iloc[:17, :]
+    base_table.iloc[:, 1:] = base_table.iloc[:, 1:].apply(pd.to_numeric)
+
+    column_list = base_table[column].tolist()
+    return base_table, column_list
+
+
+def create_final_table(base_table, target_column, population):
+    ordered_population = []
+    for idx in range(len(base_table[target_column])):
+        ordered_population.append(population[base_table[target_column][idx]])
+
+    base_table["population"] = ordered_population
+    base_table["%_firstTot"] = (base_table["firstTot"] / base_table["population"]) * 100
+    base_table["%_secondTot"] = (base_table["secondTot"] / base_table["population"]) * 100
+    base_table["upd_date"] = upd_date
+    base_table.columns = [
+        "sido",
+        "new_injected_1st",
+        "total_injected_1st",
+        "new_injected_2nd",
+        "total_injected_2nd",
+        "population",
+        "percent_total_injected_1st",
+        "percent_total_injected_2nd",
+        "upd_date",
+    ]
+    final_table = base_table.copy()
+    return final_table
+
+
+def get_kr_population(url, data_list):
+    results = requests.get(url).json()
+    population = {}
+    for i in range(len(results)):
+        if len(results[i]["C1"]) == 2:
+            if results[i]["C1_NM"] in data_list:
+                population[results[i]["C1_NM"]] = int(results[i]["DT"])
+    return population
+
+
+# 공공데이터포털-질병관리청_코로나19 예방접종 현황: https://www.data.go.kr/data/15078166/openapi.do
+sido_vaccine_api = "https://nip.kdca.go.kr/irgd/cov19stats.do?list=sido"
+
+# KOSIS공유서비스: https://kosis.kr/openapi/index/index.jsp
+kr_population_api = f"https://kosis.kr/openapi/Param/statisticsParameterData.do?method=getList&apiKey={kosis_apiKey}=&itmId=T20+&objL1=ALL&objL2=&objL3=&objL4=&objL5=&objL6=&objL7=&objL8=&format=json&jsonVD=Y&prdSe=M&newEstPrdCnt=1&loadGubun=2&orgId=101&tblId=DT_1B040A3"
+
+"""MAIN PROCESS"""
+todayDate = datetime.today().date()
+
+try:
+    if check_db_update() != todayDate:
+        sido_data = get_data_from_xml(sido_vaccine_api)
+        upd_date = get_update_date(sido_data)
+
+        if upd_date == todayDate:
+            base_table, sido_list = create_base_table(sido_data, "sidoNm")
+
+            kr_population = get_kr_population(kr_population_api, sido_list)
+
+            final_table = create_final_table(base_table, "sidoNm", kr_population)
+
+            upload_table(final_table)
+    else:
+        print(f"{todayDate} 백신 접종 현황 데이터는 이미 업데이트 되어 있습니다.")
+except Exception as e:
+    print(e)
